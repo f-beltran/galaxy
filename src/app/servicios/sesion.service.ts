fe4d6a7e494839ff0dfeaf957418586fa0233924
Franz Beltran <fbeltran@ucb.edu.bo>
2022-10-23 10:00:47 -0400

    Apuntar a produccion

--- src/app/servicios/sesion.service.ts
+++ src/app/servicios/sesion.service.ts
@@ -10,8 +10,14 @@ export class SesionService {
 
   constructor(private http: HttpClient) { }
 
+  // iniciarSesion(payload: any): Observable<RespuestaSesion>{
+  //   return  this.http.post<RespuestaSesion>('http://192.168.0.111/BackendTareas/Login', payload).pipe(
+  //     tap( (e) => localStorage.setItem('token', e.datos.token) )
+  //   )
+  // }
+
   iniciarSesion(payload: any): Observable<RespuestaSesion>{
-    return  this.http.post<RespuestaSesion>('http://192.168.0.111/BackendTareas/Login', payload).pipe(
+    return  this.http.post<RespuestaSesion>('https://backend.cba.ucb.edu.bo/BETareas/Login', payload).pipe(
       tap( (e) => localStorage.setItem('token', e.datos.token) )
     )
   }
--- src/app/servicios/tareas.service.ts
+++ src/app/servicios/tareas.service.ts
@@ -10,16 +10,28 @@ export class TareasService {
 
   constructor( private http: HttpClient) { }
 
+  // obtenerTareas(): Observable<ListaTareas>{
+  //   return this.http.get<ListaTareas>('http://192.168.0.111/BackendTareas/Tareas/Lista')
+  // }
+  // crearTarea(payload: any): Observable<RespuestaSesion>{
+  //   return this.http.post<RespuestaSesion>('http://192.168.0.111/BackendTareas/Tareas/Guardar', payload)
+  // }
+  // cambiarEstado(payload: any){
+  //   return this.http.post<RespuestaSesion>('http://192.168.0.111/BackendTareas/Tareas/EditarEstado', payload)
+  // }
+  // eliminarTarea(payload: any){
+  //   return this.http.post<RespuestaSesion>('http://192.168.0.111/BackendTareas/Tareas/Eliminar', payload)
+  // }
   obtenerTareas(): Observable<ListaTareas>{
-    return this.http.get<ListaTareas>('http://192.168.0.111/BackendTareas/Tareas/Lista')
+    return this.http.get<ListaTareas>('https://backend.cba.ucb.edu.bo/BETareas/Tareas/Lista')
   }
   crearTarea(payload: any): Observable<RespuestaSesion>{
-    return this.http.post<RespuestaSesion>('http://192.168.0.111/BackendTareas/Tareas/Guardar', payload)
+    return this.http.post<RespuestaSesion>('https://backend.cba.ucb.edu.bo/BETareas/Tareas/Guardar', payload)
   }
   cambiarEstado(payload: any){
-    return this.http.post<RespuestaSesion>('http://192.168.0.111/BackendTareas/Tareas/EditarEstado', payload)
+    return this.http.post<RespuestaSesion>('https://backend.cba.ucb.edu.bo/BETareas/Tareas/EditarEstado', payload)
   }
   eliminarTarea(payload: any){
-    return this.http.post<RespuestaSesion>('http://192.168.0.111/BackendTareas/Tareas/Eliminar', payload)
+    return this.http.post<RespuestaSesion>('https://backend.cba.ucb.edu.bo/BETareas/Tareas/Eliminar', payload)
   }
 }
